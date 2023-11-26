11bc576915810f6a75f254ba574f4afa551de1d1
Adem Kedir <ademkedir724@gmail.com>
2023-11-26 17:45:02 +0300

    create fully functional game that work on console
    have function take player selection, and create for a computer
    a selection
    have a function to identify who win each single game
    have a function that allow to play five times simultaneously
    have also a while loop that control wrong instance of input

---
+++ work.js
@@ -0,0 +1,62 @@
+console.log("js is connected to index html");
+
+const gameValue = ["rock", "paper", "scissors"];
+
+// getComputerChoice
+
+// function to check who win the game
+function playGame(computerSelection, playerSelection) {
+  let winOrLose;
+  if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      winOrLose = "You lose";
+    } else if (computerSelection === "paper") {
+      winOrLose = "You win";
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      winOrLose = "You win";
+    } else if (computerSelection === "scissors") {
+      winOrLose = "you lose";
+    }
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      winOrLose = "you lose";
+    } else if (computerSelection === "scissors") {
+      winOrLose = "you win";
+    } else {
+      winOrLose = "draw";
+    }
+  }
+  return winOrLose;
+}
+
+// game() plays five times then display the winner
+function game() {
+  let count = 1;
+  while (count <= 5) {
+    const computerSelection = gameValue[Math.floor(Math.random() * 3)];
+    console.log("computer selection " + computerSelection);
+    let playerSelection = prompt(
+      `Please choose one: ${gameValue.slice(0, 2).join(", ")} or ${
+        gameValue[2]
+      }`
+    ).toLocaleLowerCase();
+    while (!gameValue.includes(playerSelection)) {
+      playerSelection = prompt(
+        `${playerSelection} is not valid!!!
+    Please choose one again: ${gameValue.slice(0, 2).join(", ")} or ${
+          gameValue[2]
+        }`
+      ).toLocaleLowerCase();
+    }
+    console.log("player selection " + playerSelection);
+    playerWOrL = playGame(computerSelection, playerSelection);
+    console.log("player: " + playerWOrL);
+    console.log("*****************************");
+    count++;
+  }
+}
+
+// calling a game to start
+game();
