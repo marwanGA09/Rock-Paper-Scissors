40daa2a25006039a954addecc1a76d5a5efb9d64
Adem Kedir <ademkedir724@gmail.com>
2024-01-15 15:40:32 +0300

    change how to win the game(logic of game)
    - to win a game one should have 3 wining score before the opponent

--- work.js
+++ work.js
@@ -46,7 +46,11 @@ const computerContainer = document.querySelector(".player-two-choice");
 const computerImg = document.querySelector(".player-two-choice img");
 
 const winOrLose = document.querySelector(".winner-display");
-const winOrLoseText = document.querySelector(".winner-display  h2");
+const playerOneCount = document.querySelector(".p-one");
+const playerTwoCount = document.querySelector(".p-two");
+const midSpan = document.querySelector(".mid-span");
+let oneCount = 0,
+  twoCount = 0;
 const replayGame = document.querySelector(".replay-btn");
 const exitBtn = document.querySelector(".exit");
 
@@ -64,6 +68,10 @@ playBtn.addEventListener("click", (event) => {
   playBtn.textContent = `Select one from the left \n <~~`;
 
   playerOne.addEventListener("click", (event) => {
+    if (oneCount == 0 || twoCount == 0) {
+      midSpan.textContent = "-";
+    }
+
     let computerSelection = gameValue[Math.floor(Math.random() * 3)];
     let playerSelection;
     let card = event.target.getAttribute("id");
@@ -94,17 +102,33 @@ playBtn.addEventListener("click", (event) => {
     computerContainer.classList.remove("hidden");
     playBtn.classList.add("hidden");
     winOrLose.classList.remove("hidden");
-    replayGame.classList.remove("hidden");
     exitBtn.classList.remove("hidden");
     let result = playGame(computerSelection, playerSelection);
-    winOrLoseText.textContent = result;
+
+    // playerOneCount.textContent = result;
 
     if (result == "You Win") {
       winOrLose.setAttribute("style", "background-color:rgb(0, 189, 0)");
+      oneCount += 1;
     } else if (result == "You Lose") {
       winOrLose.setAttribute("style", "background-color: rgb(255, 0, 0)");
+      twoCount += 1;
     } else if (result == "Draw") {
       winOrLose.setAttribute("style", "background-color:#ffff00");
+    }
+
+    playerOneCount.textContent = oneCount;
+    playerTwoCount.textContent = twoCount;
+
+    if (oneCount == 3 || twoCount == 3) {
+      if (oneCount == 3) {
+        midSpan.textContent = "You Win The Game!!!";
+      } else {
+        midSpan.textContent = "Sorry You Lose The Game";
+      }
+      oneCount = twoCount = 0;
+
+      replayGame.classList.remove("hidden");
     }
   });
 });
